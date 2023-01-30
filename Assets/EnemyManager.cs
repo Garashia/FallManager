57a302b3cf9f354006db3b175116cef653a03e3d
TSITCL\it222256 <10ka.19.396.m@gmail.com>
2023-01-30 15:24:13 +0900

    他のプログラムも追加

---
+++ Assets/EnemyManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    private GameObject target;
+    [SerializeField]
+    private float ScopeSizeRight;
+    [SerializeField]
+    private float ScopeSizeLeft;
+
+    [SerializeField]
+    private float speed;
+
+
+    bool oikake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+        oikake = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < target.transform.position.x + ScopeSizeRight)
+        {
+            if (transform.position.x > target.transform.position.x - ScopeSizeLeft)
+            {
+                oikake = true;
+            }
+
+        }
+        if(oikake)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+
+        }
+    }
+}
